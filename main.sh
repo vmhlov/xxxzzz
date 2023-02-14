6abacffb061cbee6f838429854c5d3e333bde13d
vmhlov <30536717+vmhlov@users.noreply.github.com>
2023-02-14 10:53:24 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,13 +3,14 @@
 # Download and install Trojan-go
 mkdir /tmp/trojan-go
 wget -O /tmp/trojan-go/trojan-go.zip https://github.com/p4gefau1t/trojan-go/releases/latest/download/trojan-go-linux-amd64.zip
-unzip /tmp/trojan-go/trojan-go.zip -d /tmp/trojan-go
-install -m 0755 /tmp/trojan-go/trojan-go /usr/local/bin/trojan-go
+busybox unzip /tmp/trojan-go/trojan-go.zip -d /tmp/trojan-go
+install -m 755 /tmp/trojan-go/trojan-go /usr/local/bin/trojan-go
 
 # Remove temporary directory
 rm -r /tmp/trojan-go
 
 # Trojan-go new configuration
+mkdir -p /etc/trojan-go/ /usr/local/etc/trojan-go
 install -d /usr/local/etc/trojan-go
 cat << EOF > /usr/local/etc/trojan-go/config.yaml
 run-type: server
